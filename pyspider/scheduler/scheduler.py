afe615f2b9bf967c34ecfc85e39778d8e38fe476
dulci <sqk_king@sina.com>
2019-02-04 18:12:58 +0800

    1.scheduler缓慢启动后续抓取

--- pyspider/scheduler/scheduler.py
+++ pyspider/scheduler/scheduler.py
@@ -833,6 +833,7 @@ class Scheduler(object):
     def on_new_request(self, task):
         '''Called when a new request is arrived'''
         task['status'] = self.taskdb.ACTIVE
+        time.sleep(0.1)
         self.insert_task(task)
         self.put_task(task)
 
