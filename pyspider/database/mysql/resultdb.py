4f7251ae19aa6bdb04540d21ee3733696755e70f
dulci <sqk_king@sina.com>
2019-04-18 15:50:31 +0800

    1.clean功能追加详情记录表

--- pyspider/database/mysql/resultdb.py
+++ pyspider/database/mysql/resultdb.py
@@ -183,5 +183,7 @@ class ResultDB(MySQLMixin, SplitTableMixin, BaseResultDB, BaseDB):
     def clean(self, project):
         tablename = "crawler_result_record"
         self._delete(tablename, where="project = %s", where_values=[project])
+        tablename = "crawler_content_result_record"
+        self._delete(tablename, where="project = %s", where_values=[project])
         tablename = "completion_delay_monitoring_record"
         self._delete(tablename, where="code = %s", where_values=[project])
