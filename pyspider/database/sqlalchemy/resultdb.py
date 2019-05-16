d4862f7f429094deeee65e69d92c9d18a89e90b3
zhuyf-b <34266445@qq.com>
2019-05-16 11:39:49 +0800

    修改sqlalchemy中的resultdb

--- pyspider/database/sqlalchemy/resultdb.py
+++ pyspider/database/sqlalchemy/resultdb.py
@@ -12,20 +12,68 @@ import json
 import sqlalchemy.exc
 
 from sqlalchemy import (create_engine, MetaData, Table, Column,
-                        String, Float, LargeBinary)
+                        String, Float, LargeBinary, Integer, DateTime, and_, or_)
 from sqlalchemy.engine.url import make_url
+# from sqlalchemy.orm import sessionmaker
 from pyspider.database.base.resultdb import ResultDB as BaseResultDB
 from pyspider.libs import utils
 from .sqlalchemybase import SplitTableMixin, result2dict
 
+completion_delay_monitoring_record = Table('completion_delay_monitoring_record', MetaData(),
+                        Column('id', Integer, primary_key=True, nullable=False),
+                        Column('crawler_team_id', Integer),
+                        Column('code', String(200)),
+                        Column('website_type', Integer),
+                        Column('title', String(200)),
+                        Column('md5', String(50)),
+                        Column('publish_date', DateTime),
+                        Column('crawler_time', DateTime),
+                        Column('upload_time', DateTime),
+                        Column('upload_status', Integer),
+                        Column('status', Integer),
+                        Column('meddle_by', Integer),
+                        Column('meddle_time', DateTime),
+                        Column('meddle_remark', String(200)),
+                        Column('is_del', Integer),
+                        Column('deleted_at', DateTime),
+                        Column('url', String(1024)),
+                        Column('url_md5', String(50)),
+                        Column('taskid', String(64)),
+                        Column('skip_fetcher', Integer),
+                        mysql_engine='InnoDB',
+                        mysql_charset='utf8')
 
+business_miss_monitoring_record = Table('business_miss_monitoring_record', MetaData(),
+                        Column('id', Integer, primary_key=True, nullable=False),
+                        Column('source', String(50)),
+                        Column('url', String(500)),
+                        Column('title', String(200)),
+                        Column('publish_date', DateTime),
+                        Column('crawler_time', DateTime),
+                        Column('deal_status', Integer),
+                        Column('remark', String(500)),
+                        Column('deal_by', Integer),
+                        Column('deal_time', DateTime),
+                        Column('duty_person', String(255)),
+                        Column('keyword', String(255)),
+                        Column('website_url', String(500)),
+                        Column('info_category', String(255)),
+                        Column('is_del', Integer),
+                        Column('find_flag', Integer),
+                        Column('audit_status', Integer),
+                        Column('assist_status', Integer),
+                        Column('assist_url', String(500)),
+                        mysql_engine='InnoDB',
+                        mysql_charset='utf8')
 class ResultDB(SplitTableMixin, BaseResultDB):
     __tablename__ = ''
 
     def __init__(self, url):
         self.table = Table('__tablename__', MetaData(),
                            Column('taskid', String(64), primary_key=True, nullable=False),
+                           Column('project', String(100)),
                            Column('url', String(1024)),
+                           Column('group', String(100)),
                            Column('result', LargeBinary),
                            Column('updatetime', Float(32)),
                            mysql_engine='InnoDB',
@@ -73,25 +121,115 @@ class ResultDB(SplitTableMixin, BaseResultDB):
             data['result'] = utils.utf8(json.dumps(data['result']))
         return data
 
-    def save(self, project, taskid, url, result):
-        if project not in self.projects:
+    def save(self, project, taskid, url, result, group=None):
+        if group == 'self_crawler':
+            tablename = "crawler_content_result_record"
+        else:
+            tablename = "crawler_result_record"
+        if tablename not in self.projects:
             self._create_project(project)
             self._list_project()
-        self.table.name = self._tablename(project)
+        self.table.name = self._tablename(tablename)
         obj = {
+            'project': project,
             'taskid': taskid,
+            'group': group,
             'url': url,
             'result': result,
             'updatetime': time.time(),
         }
-        if self.get(project, taskid, ('taskid', )):
-            del obj['taskid']
-            return self.engine.execute(self.table.update()
-                                       .where(self.table.c.taskid == taskid)
-                                       .values(**self._stringify(obj)))
+        # if self.get(project, taskid, ('taskid', )):
+        #     del obj['taskid']
+        #     db_result = self.engine.execute(self.table.update()
+        #                                .where(self.table.c.taskid == taskid)
+        #                                .values(**self._stringify(obj)))
+        # else:
+        #     db_result = self.engine.execute(self.table.insert()
+        #                                .values(**self._stringify(obj)))
+        db_result = self._save(tablename, taskid, obj)
+        if group == 'self_crawler':
+            return db_result
         else:
-            return self.engine.execute(self.table.insert()
-                                       .values(**self._stringify(obj)))
+            group_name = str(group).strip()
+            if group_name == 'completion_delay_monitoring':
+                # tablename = "completion_delay_monitoring_record"
+                crawler_team_id = ""
+                website_type = ""
+                # Session = sessionmaker(bind=self.engine)
+                # session = Session()
+                try:
+                    for one in self.engine.execute("select crawler_team_id, website_type from bid_website where code='%s'"%(project)):
+                        crawler_team_id = one['crawler_team_id']
+                        website_type = one['website_type']
+                    obj = {
+                        'crawler_team_id': crawler_team_id,
+                        'code': project,
+                        'website_type': website_type,
+                        'title': str(result['title']).strip(),
+                        'crawler_time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
+                        'taskid': taskid,
+                        'url': url,
+                    }
+                    if result.get('publish_date'):
+                        if len(result['publish_date']) <= 5:
+                            publish_date = re.sub('[\[\]日]', '', re.sub('[年月/\\\]', '-', result['publish_date']))
+                            targetMonth = int(time.strftime("%m", time.strptime(publish_date.replace("\\", "-"), "%m-%d")))
+                            currentMonth = int(time.strftime("%m", time.localtime(time.time())))
+                            if (currentMonth == 11 or currentMonth == 12) and (targetMonth == 1 or targetMonth == 2):
+                                year = int(time.strftime("%Y", time.localtime(int(time.time())))) + 1
+                            else:
+                                year = int(time.strftime("%Y", time.localtime(int(time.time()))))
+                            obj['publish_date'] = str(year) + "-" + publish_date
+                        else:
+                            obj['publish_date'] = re.sub('[\[\]日]', '', re.sub('[年月]', '-', result['publish_date']))
+                    return self.other_table_save(completion_delay_monitoring_record, 'url', obj['url'], obj)
+                except Exception as e:
+                    pass
+                    # session.close()
+                finally:
+                    pass
+                    # session.close()
+            elif group_name == 'business_miss_monitoring':
+                # tablename = "business_miss_monitoring_record"
+                crawler_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                obj = {
+                    'source': project,
+                    'url': url,
+                    'title': str(result['title']).strip(),
+                    'publish_date': result['publish_date'],
+                    'crawler_time': crawler_time,
+                }
+                return self.other_table_save(business_miss_monitoring_record, 'url', obj['url'], obj)
+
+    def _save(self, project, taskid, fields):
+        if self.get(project, taskid, ('taskid', ), project):
+            del fields['taskid']
+            db_result = self.engine.execute(self.table.update()
+                                       .where(self.table.c.taskid == taskid)
+                                       .values(**self._stringify(fields)))
+        else:
+            db_result = self.engine.execute(self.table.insert()
+                                       .values(**self._stringify(fields)))
+        return db_result
+
+    def _get(self, table, column, column_value, fields=None):
+        columns = [getattr(table.c, f, f) for f in fields] if fields else table.c
+        for db_result in self.engine.execute(table.select()
+                                        .with_only_columns(columns=columns)
+                                        .where(getattr(table.c, column) == column_value)
+                                        .limit(1)):
+            return self._parse(result2dict(columns, db_result))
+
+    def other_table_save(self, table, column, column_value, fields):
+        if self._get(table, column, column_value, (column, )):
+            del fields['column']
+            db_result = self.engine.execute(table.update()
+                                       .where(getattr(table.c, column) == column_value)
+                                       .values(**self._stringify(fields)))
+        else:
+            db_result = self.engine.execute(table.insert()
+                                       .values(**self._stringify(fields)))
+        return db_result
 
     def select(self, project, fields=None, offset=0, limit=None):
         if project not in self.projects:
@@ -108,6 +246,31 @@ class ResultDB(SplitTableMixin, BaseResultDB):
                                         .execution_options(autocommit=True)):
             yield self._parse(result2dict(columns, task))
 
+    def select(self, project, group, taskid=None, url=None, upload_status=None, fields=None, offset=0, limit=None):
+        if group == 'self_crawler':
+            tablename = "crawler_content_result_record"
+        elif group == 'completion_delay_monitoring':
+            tablename = "crawler_result_record"
+        self.table.name = self._tablename(tablename)
+        whl_con = and_(self.table.c.project == project)
+        if taskid:
+            whl_con = and_(whl_con, self.table.c.taskid == taskid)
+        if url:
+            whl_con = and_(whl_con, self.table.c.url == url)
+        if upload_status:
+            status_con = None
+            for i in upload_status.split(","):
+                status_con = or_(status_con, self.table.c.upload_status == i) if status_con else or_(self.table.c.upload_status == i)
+            whl_con = and_(whl_con, status_con)
+        columns = [getattr(self.table.c, f, f) for f in fields] if fields else self.table.c
+        for task in self.engine.execute(self.table.select()
+                                                .with_only_columns(columns=columns)
+                                                .where(whl_con)
+                                                .order_by(self.table.c.updatetime.desc())
+                                                .offset(offset).limit(limit)
+                                                .execution_options(autocommit=True)):
+            yield self._parse(result2dict(columns, task))
+
     def count(self, project):
         if project not in self.projects:
             self._list_project()
@@ -118,16 +281,60 @@ class ResultDB(SplitTableMixin, BaseResultDB):
         for count, in self.engine.execute(self.table.count()):
             return count
 
-    def get(self, project, taskid, fields=None):
-        if project not in self.projects:
-            self._list_project()
-        if project not in self.projects:
-            return
-        self.table.name = self._tablename(project)
+    def count(self, project, group, taskid=None, url=None, upload_status=None):
+        if group == 'self_crawler':
+            tablename = "crawler_content_result_record"
+        elif group == 'completion_delay_monitoring':
+            tablename = "crawler_result_record"
+        self.table.name = self._tablename(tablename)
+        whl_con = and_(self.table.c.project == project)
+        if taskid:
+            whl_con = and_(whl_con, self.table.c.taskid == taskid)
+        if url:
+            whl_con = and_(whl_con, self.table.c.url == url)
+        if upload_status:
+            status_con = None
+            for i in upload_status.split(","):
+                status_con = or_(status_con, self.table.c.upload_status == i) if status_con else or_(
+                    self.table.c.upload_status == i)
+            whl_con = and_(whl_con, status_con)
+        for count, in self.engine.execute(self.table.count().where(whl_con)):
+            return count
+
+    def get(self, project, taskid, fields=None, table_name='crawler_result_record'):
+        # if project not in self.projects:
+        #     self._list_project()
+        # if project not in self.projects:
+        #     return
+
+        self.table.name = self._tablename(table_name)
 
         columns = [getattr(self.table.c, f, f) for f in fields] if fields else self.table.c
         for task in self.engine.execute(self.table.select()
                                         .with_only_columns(columns=columns)
-                                        .where(self.table.c.taskid == taskid)
+                                        .where(and_(self.table.c.taskid == taskid, self.table.c.project == project))
                                         .limit(1)):
             return self._parse(result2dict(columns, task))
+
+    def get_content(self, project, taskid, fields=None, table_name='crawler_content_result_record'):
+        self.table.name = self._tablename(table_name)
+
+        columns = [getattr(self.table.c, f, f) for f in fields] if fields else self.table.c
+        for task in self.engine.execute(self.table.select()
+                                                .with_only_columns(columns=columns)
+                                                .where(
+            and_(self.table.c.taskid == taskid, self.table.c.project == project))
+                                                .limit(1)):
+            return self._parse(result2dict(columns, task))
+
+    def clean(self, project, group):
+        if group == 'self_crawler':
+            self.table.name = "crawler_content_result_record"
+            return self.engine.execute(self.table.delete()
+                                       .where(self.table.c.project == project))
+        elif group == 'completion_delay_monitoring':
+            self.table.name = "crawler_result_record"
+            return self.engine.execute(self.table.delete()
+                                       .where(self.table.c.project == project))
+            return self.engine.execute(completion_delay_monitoring_record.delete()
+                                       .where(self.table.c.project == project))
