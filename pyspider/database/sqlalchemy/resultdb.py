bc8b71dd1b7f7736aa6c350da45be33019481ad0
dulci <sqk_king@sina.com>
2019-05-20 09:27:57 +0800

    1.sleep

--- pyspider/database/sqlalchemy/resultdb.py
+++ pyspider/database/sqlalchemy/resultdb.py
@@ -314,7 +314,7 @@ class ResultDB(SplitTableMixin, BaseResultDB):
             return self._parse(result2dict(columns, task))
 
     def get_content(self, project, taskid, fields={'taskid', 'project', 'url'}, table_name='crawler_content_result_record'):
-        time.sleep(0.1)
+        time.sleep(0.2)
         self.table.name = self._tablename(table_name)
         columns = [getattr(self.table.c, f, f) for f in fields] if fields else self.table.c
         for task in self.engine.execute(self.table.select()
