1d133500a4fac6b54a0a36a91f10bdcfe190ab5f
dulci <sqk_king@sina.com>
2019-05-20 10:21:07 +0800

    1.非空检验

--- pyspider/scheduler/scheduler.py
+++ pyspider/scheduler/scheduler.py
@@ -634,9 +634,9 @@ class Scheduler(object):
     def _load_put_task(self, project, taskid):
         try:
             task = self.taskdb.get_task(project, taskid, fields=self.request_task_fields)
-            if task.get('page_num') == None:
+            if task is not None and task.get('page_num') == None:
                 task['page_num'] = 0
-            if task.get('skip_fetcher') == None:
+            if task is not None and task.get('skip_fetcher') == None:
                 task['skip_fetcher'] == False
         except ValueError:
             logger.error('bad task pack %s:%s', project, taskid)
