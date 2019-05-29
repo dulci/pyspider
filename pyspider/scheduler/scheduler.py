1f1bbec6912cb88da7d065316b4b2dd7b1c552f5
zhuyf-b <34266445@qq.com>
2019-05-29 17:42:14 +0800

    打印日志

--- pyspider/scheduler/scheduler.py
+++ pyspider/scheduler/scheduler.py
@@ -388,14 +388,17 @@ class Scheduler(object):
                 return
 
         try:
+            if task.get('fetch', {}).get('fetch_type') ==  'webdriver':
+                logger.debug("%s webdriver task's schedule queue name is %s"%(task['project'], task.get("schedule", {}).get('queue_name')))
+                logger.debug("%s webdriver task's out_queue is %s"%(task['project'], task.get("schedule", {}).get('queue_name')))
+
             if task.get('schedule', {}).get('queue_name'):
                 out_queue = [x for x in self.out_queues if x.name == task.get('schedule', {}).get('queue_name')][0]
             else:
                 #for one in self.out_queues:
                     #logger.info('queue name %s, message count %d'%(one.name, one.qsize()))
                 out_queue = sorted(self.out_queues, key=lambda x:x.qsize())[0]
-            if task.get('fetch', {}).get('fetch_type') ==  'webdriver':
-                logger.debug("%s webdriver task's out_queue is %s"%(task['project'], task.get("schedule", {}).get('queue_name')))
+
             if task['taskid'] == 'on_start':
                 task['schedule']['queue_name'] = out_queue.name
             if task['taskid'] == '_on_cronjob':
